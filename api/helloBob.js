20256474ea42105f34175b09e6bfc86caad979c1
lena.s <sanolenaa@gmail.com>
2023-05-28 17:33:53 -0700

    remove delay in async serverless function

--- api/helloBob.js
+++ api/helloBob.js
@@ -1,5 +1,5 @@
 export async function sayHello(person) {
-  const delay = person === 'Bob' ? 2000 : 200;
+  const delay = person === 'Bob' ? 0: 0; // 2000 : 200;
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(person + ' says hello.');
