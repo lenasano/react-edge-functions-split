3ccfb1e45ba3e51485da86b7edefcd86a0673a4f
lena.s <sanolenaa@gmail.com>
2023-05-29 09:12:42 -0700

    export get function in spi/split/flag

--- api/split/flag.ts
+++ api/split/flag.ts
@@ -9,7 +9,8 @@ const FEATURE_FLAG_NAME = 'test_split';
 // Run API route as an Edge function rather than a Serverless one, because the SDK uses Fetch API to flush data, which is available in Edge runtime but not in Serverless.
 export const config = { runtime: "edge" };
 
-export async function getFlag(): string {
+
+export async function get(): string {
 
     // instantiate the SDK
     const factory = SplitFactory({
@@ -43,33 +44,7 @@ export async function getFlag(): string {
 
 export default async function handler(req) {
 
-    // instantiate the SDK
-    const factory = SplitFactory({
-        core: {
-            authorizationKey: process.env.NEXT_PUBLIC_SPLIT_SDK_KEY_STNDALN,
-            // key represents your internal user id, or the account id that
-            // the user belongs to.
-            // This could also be a cookie you generate for anonymous users
-            key: 'doesnt_matter_getting_default_treatment'
-        },
-        debug: "INFO"
-    })
-
-    // and get the client instance
-    const client = factory.client()
-
-    console.log(`hello from split: ${client !== null}`)
-
-    await client.ready()
-
-    console.log("split is ready")
-
-    const treatment = await client.getTreatment("first_split")
-
-    console.log(`split result is ${treatment}`)
-
-    client.destroy()
-
+    const treatment = await get()
 
     return new Response(JSON.stringify({ treatment }), {
         status: 200,
