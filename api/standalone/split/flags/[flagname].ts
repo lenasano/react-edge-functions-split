1bb3aaa57e2cc2bcec337b602b2ef54ddc0c81d0
lena.s <sanolenaa@gmail.com>
2023-05-31 06:33:08 -0700

    remove logging, next: add server-side rendering for <Split> component

--- api/standalone/split/flags/[flagname].ts
+++ api/standalone/split/flags/[flagname].ts
@@ -35,13 +35,11 @@ export async function get(flagname: string, timer?: Timer): Promise<string> {
 
     const treatment = await client.getTreatment( flagname )
 
-    if (timer) console.log( `time is ${timer.stop()}` )
+    if (timer) timer.stop()
 
     console.log(`split result is ${treatment}`)
 
     ne !== null ? ne.waitUntil(client.destroy()) : await client.destroy()
-
-    console.log(`duration is ${timer.duration()}`)
 
     return treatment
 }
@@ -59,8 +57,6 @@ export default async function handler(req: NextRequest, event: NextFetchEvent) {
     const treatment = await get(flagname, stopwatch);
 
     ne = null;
-
-    console.log(`response is ${JSON.stringify({ treatment })} and ${JSON.stringify({ duration: stopwatch.duration() })}`);
 
     return new Response(JSON.stringify({ treatment, duration: stopwatch.duration() }), {
         status: 200,
