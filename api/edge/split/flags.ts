4adaf6ca607bbb28bf3ab8e1c94f53d83c2292bf
lena.s <sanolenaa@gmail.com>
2023-06-04 10:58:10 -0700

    remove api folder to check if routing/caching is affected (i.e. is city and ip address 'mine'?

--- api/edge/split/flags.ts
+++
@@ -1,8 +0,0 @@
-export default async function handler(req) {
-    return new Response(JSON.stringify('"op": "not implemented"'), {
-        status: 200,
-        headers: { 'content-type': 'application/json' }
-    });
-}
-
-// https://github.com/NicoZelaya/demo/blob/main/pages/api/flags/index.ts
--- api/standalone/split/flags.ts
+++
@@ -1,6 +0,0 @@
-export default async function handler(req) {
-    return new Response(JSON.stringify('"op": "not implemented"'), {
-        status: 200,
-        headers: { 'content-type': 'application/json' }
-    });
-}
