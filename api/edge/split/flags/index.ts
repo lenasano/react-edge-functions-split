cf1c3211e356f53754e16b272ec33dd76531c6db
lena.s <sanolenaa@gmail.com>
2023-06-23 12:49:23 -0700

    return plain text

--- api/edge/split/flags/index.ts
+++ api/edge/split/flags/index.ts
@@ -7,8 +7,8 @@ export default async function handler(req: NextRequest) {
     // It is possible to retrieve the results of n feature flags by using the SplitFactory.client().getTreatments() function.
     // See https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#multiple-evaluations-at-once.
 
-    return new Response(JSON.stringify('{"op": "not implemented"}'), {
+    return new Response("ok", {
         status: 200,
-        headers: { 'content-type': 'application/json' }
+        headers: { 'content-type': 'text/plain' }
     });
 }
