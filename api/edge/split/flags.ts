c1586ff1b3547759042a548a963795e4ef8b2f2c
lena.s <sanolenaa@gmail.com>
2023-06-23 12:38:31 -0700

    add NextRequest

--- api/edge/split/flags.ts
+++ api/edge/split/flags.ts
@@ -1,4 +1,6 @@
-export default async function handler(req) {
+import { NextRequest } from "next/server";
+
+export default async function handler(req: NextRequest) {
 
     // This is a stub.
     //
